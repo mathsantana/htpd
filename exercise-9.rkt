0ccc17b5d527d5ad1ca084a8955a1d84a42bb8dd
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2022-12-19 22:04:40 -0300

    adding exercises 9 and 10

---
+++ exercise-9.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-9) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define in -42)
+
+(define convert-to-non-negative
+  (cond
+  [ (string? in) (string-length in) ]
+  [ (image? in) (* (image-width in) (image-height in)) ]
+  [ (number? in) (abs in) ]
+  [ (and (boolean? in) in) 10 ]
+  [ (and (boolean? in) (not in)) 20 ])
+)
+
+convert-to-non-negative
