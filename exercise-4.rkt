4c95a7736b376a9e97acedc72dfdc784476cc2f2
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2022-12-19 22:38:46 -0300

    adding headers to .rkt files

--- exercise-4.rkt
+++ exercise-4.rkt
@@ -1,5 +1,9 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
+
+;; I Fixed Data - Exercise 4
+;; Author: @mathsantana
+
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-4) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 (define str "helloworld")
 (define i 5)
--- exercise-5.rkt
+++ exercise-5.rkt
@@ -1,6 +1,10 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-5) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+;; I Fixed Data - Exercise 5
+;; Author: @mathsantana
+
 (define SCALE 5)
 
 (overlay/xy
