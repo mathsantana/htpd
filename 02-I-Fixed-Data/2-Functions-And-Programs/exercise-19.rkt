dc7ab9f67ddc65121b3fc8b0859fbf0dff96f01c
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2022-12-20 22:12:31 -0300

    adding exercises 13-20

---
+++ 02-I-Fixed-Data/2-Functions-And-Programs/exercise-19.rkt
@@ -0,0 +1,15 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-19) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+;; I Fixed Data - Exercise 19
+;; Author: @mathsantana
+
+(define (string-insert str i)
+  (if
+   (> (string-length str) 0)
+   (string-append
+    (substring str 0 i)
+    "_"
+    (substring str i))
+   "_"))
---
+++ 02-I-Fixed-Data/2-Functions-And-Programs/exercise-20.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise-20) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+
+;; I Fixed Data - Exercise 20
+;; Author: @mathsantana
+
+(define (string-delete str i)
+  (if (> (string-lenght str) 0)
+      (string-append (substring str 0 i) (substring (+ i 1)))
+      (error "string is empty")))
